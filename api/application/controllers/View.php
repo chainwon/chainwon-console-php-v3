50be94d44e02f25c3338c5c955274b05c6940fce
折影轻梦 <776194970@qq.com>
2019-03-03 13:13:21 +0800

    233

--- api/application/controllers/View.php
+++ api/application/controllers/View.php
@@ -27,130 +27,18 @@ class View extends CI_Controller {
         return $ip;
     }
 
-    public function user(){
-        if(isset($this->Model->user)){
-            echo json_encode($this->Model->user);
-        }else{
-            echo '{"state":0,"qq":"776194970","name":"\u6298\u5f71\u8f7b\u68a6","avatar":"https:\/\/avatar.dawnlab.me\/qq\/776194970"}';
-        }
-    }
-
-    public function category($cid=NULL) {
-        if( !isset($cid) ){
-            $query = $this->db->get('category');
-            $array = $query->result_array();
-            echo json_encode($array);
-        }else{
-            $query = $this->db->get('category');
-            $this->db->where('cid',$cid);
-            $array = $query->row_array();
-
-            $this->db->order_by('time', 'DESC');
-            $this->db->where('cid',$cid);
-            $query = $this->db->get('message');
-            $array_message = $query->result_array();
-            $array_message = $this->Model->filter($array_message);
-            $a = array(
-                'category' => $array,
-                'message' => $array_message, 
-            );
-            echo json_encode($a);
-        }
-    }
-
-    public function message($mid=NULL) {
-        if( !isset($mid) ){
-            $this->db->order_by('time', 'DESC');
-            $query = $this->db->get('message');
-            $array = $query->result_array();
-            $array = $this->Model->filter($array);
-        }else{
-            $this->db->where('mid',$mid);
-            $query = $this->db->get('message');
-            $array = $query->row_array();
-            $array['comment'] = $this->comment($mid);
-            $array = $this->Model->filter($array,true);
-        }
-        echo json_encode($array);
-    }
-
-    public function comment($mid){
-        $this->db->where('mid',$mid);
-        $this->db->where('parent',0);
-        $query = $this->db->get('comment');
-        $array = $query->result_array();
-        $this->count += count($array);
-        $x=0;
-        while( isset($array[$x]['text']) ){
-            $array[$x]['author'] = $this->Model->qq($array[$x]['author']);
-            $array[$x]['time'] = date('n月j日 H:i',$array[$x]['time']);
-            $array[$x]['children'] = $this->comment_children($array[$x]['coid']);
-            $x++;
-        }
-        return array(
-            'count' => $this->count,
-            'data' => $array,
-        );
-    }
-
-    public function comment_children($coid){
-        $this->db->where('parent',$coid);
-        $query = $this->db->get('comment');
-        $array = $query->result_array();
-        $this->count += count($array);
-        $x=0;
-        while( isset($array[$x]['text']) ){
-            $array[$x]['author'] = $this->Model->qq($array[$x]['author']);
-            $array[$x]['time'] = date('n月j日 H:i',$array[$x]['time']);
-            $array[$x]['children'] = $this->comment_children($array[$x]['coid']);
-            $array[$x]['parent'] = $this->reply($array[$x]['parent']);
-            $x++;
-        }
-        return $array;
-    }
-
-    public function reply($coid){
-        $this->db->where('coid',$coid);
-        $query = $this->db->get('comment');
-        $array = $query->row_array();
-        return $this->Model->qq($array['author']);
-    }
-
-    public function notice($count=false){
-
-        if(!isset($this->Model->user)){
-            $this->Model->end('');
-        }
-
-        $uid = $this->Model->user['qq'];
-        if(!$count){
-            $this->db->where('uid',$uid);
-            $this->db->order_by('time', 'DESC');
-            $this->db->select('type,time,mid,coid,read');
-            $query = $this->db->get('notice');
-            $array = $query->result_array();
-            $x=0;
-            while(isset($array[$x])){
-                $this->db->where('coid',$array[$x]['coid']);
-                $this->db->select('author,text');
-                $query = $this->db->get('comment');
-                $array[$x]['origin'] = $query->row_array();
-                $array[$x]['origin']['author'] = $this->Model->qq($array[$x]['origin']['author']);
-                $x++;
+    public function storeNavigation(){
+        $this->db->select('site_id,name,logo,intro,site,order,cover');
+        $query = $this->db->get('website');
+        $result = $query->result_array();
+        $x = 0;
+        while(isset($result[$x]['logo'])){
+            $result[$x]['logo'] = 'https://console.chainwon.com/static/img/logo/'.$result[$x]['logo'].'.png';
+            if($result[$x]['cover'] == NULL){
+                $result[$x]['cover'] = 'https://i.loli.net/2018/02/13/5a8302bdbadaa.jpg';
             }
-
-            $this->Model->end($array);
-        }else{
-            $this->db->where('uid',$uid);
-            $this->db->where('read',false);
-            $this->db->from('notice');
-            echo $this->db->count_all_results();
+            $x += 1;
         }
-    }
-
-    public function music(){
-        header('Content-type: text/html');
-        $data['url'] = $_GET['url'];
-        $this->load->view('music', $data);
+        $this->Model->end($result);
     }
 }
---
+++ api/application/logs/log-2019-03-03.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-03-03 04:43:49 --> 404 Page Not Found: View/navagition
+ERROR - 2019-03-03 04:43:55 --> Severity: Warning --> mysqli::real_connect(): (HY000/1044): Access denied for user 'chainwon'@'%' to database 'wall' D:\Work\console-php\api\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-03-03 04:43:55 --> Unable to connect to the database
+ERROR - 2019-03-03 04:44:02 --> Severity: Warning --> mysqli::real_connect(): (HY000/1044): Access denied for user 'chainwon'@'%' to database 'wall' D:\Work\console-php\api\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-03-03 04:44:02 --> Unable to connect to the database
+ERROR - 2019-03-03 04:47:41 --> Severity: error --> Exception: Call to undefined method View::select() D:\Work\console-php\api\application\controllers\View.php 32

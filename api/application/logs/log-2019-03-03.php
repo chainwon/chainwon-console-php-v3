dd1048a4e77b6a790f13b2a9131a2b5a3472bb73
折影轻梦 <776194970@qq.com>
2019-03-17 16:15:31 +0800

    Update log-2019-03-03.php

--- api/application/logs/log-2019-03-03.php
+++ api/application/logs/log-2019-03-03.php
@@ -6,3 +6,41 @@ ERROR - 2019-03-03 04:43:55 --> Unable to connect to the database
 ERROR - 2019-03-03 04:44:02 --> Severity: Warning --> mysqli::real_connect(): (HY000/1044): Access denied for user 'chainwon'@'%' to database 'wall' D:\Work\console-php\api\system\database\drivers\mysqli\mysqli_driver.php 201
 ERROR - 2019-03-03 04:44:02 --> Unable to connect to the database
 ERROR - 2019-03-03 04:47:41 --> Severity: error --> Exception: Call to undefined method View::select() D:\Work\console-php\api\application\controllers\View.php 32
+ERROR - 2019-03-03 05:36:53 --> 404 Page Not Found: View/navigation
+ERROR - 2019-03-03 05:46:43 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 05:47:30 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 05:55:47 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 05:56:07 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 05:56:29 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 05:57:27 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:02:29 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:02:30 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:04:13 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:04:14 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:06:10 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:06:11 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:20:41 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:20:42 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:21:29 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:21:30 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:21:37 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:21:38 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:21:39 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:21:40 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:22:47 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:22:48 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:22:51 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:22:52 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:22:53 --> Severity: Notice --> Undefined index: pid D:\Work\console-php\api\application\controllers\Controller.php 117
+ERROR - 2019-03-03 06:30:50 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:31:38 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:32:07 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:32:40 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:33:08 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:33:25 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:34:03 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:34:16 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:34:17 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:34:24 --> Severity: Notice --> Undefined offset: 0 D:\Work\console-php\api\application\models\Sina.php 63
+ERROR - 2019-03-03 06:35:05 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108
+ERROR - 2019-03-03 06:35:22 --> Severity: Notice --> Undefined index: file D:\Work\console-php\api\application\controllers\Controller.php 108

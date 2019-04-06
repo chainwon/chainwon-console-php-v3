f68bc4c9e4007157709996765d2f91b360e01186
折影轻梦 <776194970@qq.com>
2019-04-07 00:15:59 +0800

    Update View.php

--- api/application/controllers/View.php
+++ api/application/controllers/View.php
@@ -27,10 +27,59 @@ class View extends CI_Controller {
         return $ip;
     }
 
-    public function storeNavigation(){
+    public function settingIndex() {
+        $this->db->where('uid',10000);
+        $query = $this->db->get('user');
+        $result = $query->row_array();
+        $result['countdown']['year'] = date('Y',$result['countdown_time']);
+        $result['countdown']['month'] = date('m',$result['countdown_time']);
+        $result['countdown']['day'] = date('d',$result['countdown_time']);
+        $this->Model->end($result);
+    }
+
+    public function settingNavigation() {
+        $this->db->where('uid',10000);
+        $this->db->select('site_id,order');
+        $this->db->order_by('site_id ASC');
+        $query = $this->db->get('relationship');
+        $order = $query->result_array();
+
         $this->db->select('site_id,name,logo,intro,site,order,cover');
+        foreach ($order as $site_id){
+            $this->db->or_where('site_id',$site_id['site_id']);
+        }
+        $this->db->order_by('site_id ASC');
         $query = $this->db->get('website');
         $result = $query->result_array();
+
+        $x = 0;
+        while(isset($result[$x]['logo'])){
+            $result[$x]['logo'] = 'https://console.chainwon.com/static/img/logo/'.$result[$x]['logo'].'.png';
+            if($result[$x]['cover'] == NULL){
+                $result[$x]['cover'] = 'https://i.loli.net/2018/02/13/5a8302bdbadaa.jpg';
+            }
+            $result[$x]['order'] = $order[$x]['order'];
+            $result[$x]['added'] = true;
+            $x += 1;
+        }
+
+        array_multisort(array_column($result,'order'),SORT_ASC,$result); // 数组排序
+
+        $this->Model->end($result);
+    }
+
+    public function storeNavigation(){
+        $post = json_decode(file_get_contents("php://input"),true);
+
+        if(!isset($post['page'])){
+            $post['page'] = 1;
+        }
+        
+        $this->db->select('site_id,name,logo,intro,site,order,cover');
+        $this->db->limit(32,($post['page']-1)*32);
+        $query = $this->db->get('website');
+        $result = $query->result_array();
+
         $x = 0;
         while(isset($result[$x]['logo'])){
             $result[$x]['logo'] = 'https://console.chainwon.com/static/img/logo/'.$result[$x]['logo'].'.png';
@@ -39,6 +88,7 @@ class View extends CI_Controller {
             }
             $x += 1;
         }
+
         $this->Model->end($result);
     }
 }

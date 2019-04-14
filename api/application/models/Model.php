8dcba2ebbd46535ddfb3abf2ca697cbf060bbfe4
折影轻梦 <776194970@qq.com>
2019-04-14 13:14:54 +0800

    用户id

--- api/application/models/Model.php
+++ api/application/models/Model.php
@@ -4,9 +4,20 @@ class Model extends CI_Model {
 
     public function __construct(){
         $this->load->database();
-        if(isset($_COOKIE['qq'])){
-            $this->user = $this->qq(base64_decode($_COOKIE['qq']));
-        }
+        $post_data = array (
+            'biao' => '*',
+            'ip' => $this->get_client_ip (),
+        );
+        $username = $_COOKIE['username'];
+        $c = $_COOKIE['c'];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://account.mixcm.com/api/view/user');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($ch, CURLOPT_COOKIE, "username=$username;c=$c");
+        $this->user=json_decode(trim(curl_exec($ch), chr(239) . chr(187) . chr(191)), true);
+        curl_close($ch);
     }
     
     public function get_client_ip (){
@@ -54,61 +65,5 @@ class Model extends CI_Model {
             echo json_encode($data);
         }
         exit;
-    }
-
-    public function qq($qq){
-        $urlPre='http://r.qzone.qq.com/fcg-bin/cgi_get_portrait.fcg?g_tk=1518561325&uins=';
-        $data=file_get_contents($urlPre.$qq);
-        $data=iconv("GBK","UTF-8",$data);
-        $pattern = '/portraitCallBack\((.*)\)/is';
-        preg_match($pattern,$data,$result);
-        $result = $result[1];
-        return array(
-            "name" => json_decode($result, true)[$qq][6],
-            "avatar" => 'https://avatar.dawnlab.me/qq/'.$qq,
-            "qq" => $qq
-        );
-    }
-
-    public function category($cid) {
-        $this->db->where('cid',$cid);
-        $query = $this->db->get('category');
-        $array = $query->row_array();
-        return $array;
-    }
-
-    public function filter($array,$single=false){
-        if(!$single){
-            $x=0;
-            while( isset($array[$x]['article']) ){
-                if($array[$x]['anonymous'] == 'true'){
-                    $array[$x]['author'] = array(
-                        "name" => '匿名用户',
-                        "avatar" => 'https://static.hdslb.com/images/akari.jpg',
-                        "qq" => "10000"
-                    );
-                }else{
-                    $array[$x]['author'] = $this->Model->qq($array[$x]['author']);
-                }
-                $array[$x]['time'] = date('n月j日 H:i',$array[$x]['time']);
-                $array[$x]['category'] = $this->Model->category($array[$x]['cid'])['name'];
-                $array[$x]['article'] = json_decode($array[$x]['article'],true);
-                $x++;
-            }
-        }else{
-            if($array['anonymous'] == 'true'){
-                $array['author'] = array(
-                    "name" => '匿名用户',
-                    "avatar" => 'https://static.hdslb.com/images/akari.jpg',
-                    "qq" => "10000"
-                );
-            }else{
-                $array['author'] = $this->Model->qq($array['author']);
-            }
-            $array['time'] = date('n月j日 H:i',$array['time']);
-            $array['category'] = $this->Model->category($array['cid'])['name'];
-            $array['article'] = json_decode($array['article'],true);
-        }
-        return $array;
     }
 }
